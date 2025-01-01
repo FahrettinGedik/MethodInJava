ca66d0d5ce03ea5e63612c0312ab6899f03db4dc
Fahrettin Gedik <fahged41@gmail.com>
2025-01-01 18:07:19 +0100

    Method of list changes was organized.

---
+++ src/MethodChallenge.java
@@ -0,0 +1,40 @@
+public class MethodChallenge {
+
+    public static void main(String[] args) {
+
+        int highScorePosition = calculateHighScorePosition(1500);
+        displayHighScorePosition("Tim", highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(1000);
+        displayHighScorePosition("Jane", highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(500);
+        displayHighScorePosition("Jill", highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(100);
+        displayHighScorePosition("Joe", highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(25);
+        displayHighScorePosition("James", highScorePosition);
+    }
+
+    public static void displayHighScorePosition(String playerName, int highScorePosition) {
+
+        System.out.println(playerName + " managed to get into position " + highScorePosition + " on the high score list");
+
+    }
+
+    public static int calculateHighScorePosition(int playerScore) {
+
+        if (playerScore >= 1000) {
+            return 1;
+        } else if (playerScore >= 500) {
+            return 2;
+        } else if (playerScore >= 100) {
+            return 3;
+        } else {
+            return 4;
+        }
+
+    }
+}
